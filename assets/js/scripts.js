c5b70cb7bfbc96b3d73dad925aa0664780b01a82
Lahcen Elfarici <lahcen@fornet.ma>
2025-08-31 14:19:43 +0100

    add all html 5

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,27 @@
     // If it exists, add the 'regle_header' class to the <header> tag
     $('header').addClass('regle_header');
   }
+    $('.notre-histoire .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 25,
+                nav: true,
+                navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+                dots: true,
+                autoplay: true,
+                autoplayTimeout: 5000,
+                autoplayHoverPause: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    768: {
+                        items: 2
+                    },
+                    1200: {
+                        items: 3
+                    }
+                }
+            });
   $('.section--5 .owl-carousel').owlCarousel({
     loop: true,
     margin: 30,
